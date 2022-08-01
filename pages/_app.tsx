e12a6e958e881176e7a854e7debcc6540f1da00d
Ghostwolf <git@paulekas.eu>
2022-08-02 01:52:03 +0300

    fix: embeds

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,20 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="description"
           content="the virtual home on the web of Paulius / ghostwolf"
         />
-        <meta name="og:image" content="https://paulekas.eu/embed.png" />
+        <meta property="og:title" content="paulekas.eu" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://paulekas.eu/" />
+        <meta property="og:image" content="https://paulekas.eu/embed.png" />
         <meta
-          name="og:description"
+          property="og:description"
           content="the virtual home on the web of Paulius / ghostwolf"
         />
-        <meta name="og:url" content="https://paulekas.eu" />
-        <meta name="og:type" content="website" />
+        <meta name="twitter:title" content="paulekas.eu" />
+        <meta
+          name="twitter:description"
+          content="the virtual home on the web of Paulius / ghostwolf"
+        />
+        <meta name="twitter:image" content="https://paulekas.eu/embed.png" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Component {...pageProps} />
