7c340d18d4c99bddd5b7517df493a53813a5343a
Ghostwolf <git@paulekas.eu>
2022-08-02 17:24:55 +0300

    refactor: music api

--- pages/api/music.ts
+++ pages/api/music.ts
@@ -1,8 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// I might need to redo this later on
-// :^)
-
 type Data = {
   listening: boolean;
   artist?: string;
@@ -17,25 +14,21 @@ export default async function handler(
   if (!process.env?.LASTFMAPI)
     return res.status(500).json({ listening: false });
 
-  let request = await fetch(
-      `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ghostwolfy&api_key=${process.env.LASTFMAPI}&format=json&limit=1`
-    ),
-    result = await request.json();
+  let result = await fetch(
+    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ghostwolfy&api_key=${process.env.LASTFMAPI}&format=json&limit=1`
+  ).then((request) => request.json());
 
   if (!result?.recenttracks?.track[0]["@attr"])
     return res.status(200).json({ listening: false });
 
-  // especially this part lulw
   res.status(200).json({
     listening: true,
     artist: result.recenttracks.track[0].artist["#text"],
     title: result.recenttracks.track[0].name,
-    url: `https://open.spotify.com/search/${result.recenttracks.track[0].artist[
-      "#text"
-    ]
-      .toLowerCase()
-      .replace(/ /g, "%20")}%20${result.recenttracks.track[0].name
-      .toLowerCase()
-      .replace(/ /g, "%20")}`,
+    url: `https://open.spotify.com/search/${encodeURIComponent(
+      result.recenttracks.track[0].artist["#text"] +
+        " " +
+        result.recenttracks.track[0].name
+    )}`,
   });
 }
